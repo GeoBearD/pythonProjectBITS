646304a656df570b1ee87239c8de4e7b3df4b08b
GeoBearD <geobeardstrateg1@gmail.com>
2023-04-21 18:59:41 +0400

    поправил однофамильцев, поломанные номера, соеденил 3 таблицы в одну функцию, перенес индексы столбцов в переменные. код по прежнему нуждается в рефакторинше!

--- main.py
+++ main.py
@@ -7,114 +7,103 @@ import re
 from prettytable import PrettyTable
 import charset_normalizer
 
-NUMBERS_OF_DIGITS_IN_PHONE = 11                           # Корректное колличество цифр в номере
-filename = './base.csv'                                  # Заданное имя файла
-url = 'https://lk.globtelecom.ru/upload/test_prog1.csv'  # Ссылка на файл
+NUMBERS_OF_DIGITS_IN_PHONE = 11  # Корректное колличество цифр в номере
+FILENAME = './base.csv'  # Заданное имя файла
+URL = 'https://lk.globtelecom.ru/upload/test_prog1.csv'  # Ссылка на файл
 
 
-def download_file():                                     # Функция для скачивания файла
-    urllib.request.urlretrieve(url, filename)
+def download_file():  # Функция для скачивания файла
+    urllib.request.urlretrieve(URL, FILENAME)
 
 
-def detect_encoding(filename: str) -> str:               # Функция, определяющая кодировку файла и норм. его
-    with open(filename, 'rb') as f:
+def detect_encoding(file: str) -> str:  # Функция, определяющая кодировку файла и норм. его
+    with open(file, 'rb') as f:
         result = charset_normalizer.detect(f.read())
         return result.get('encoding')
 
 
-def validate_phone(phone: str) -> bool:                  # Проверка номер на валидность
-    valid_numbers = r"^[0-9]+$"
-    return len(phone) == NUMBERS_OF_DIGITS_IN_PHONE and re.match(valid_numbers, phone)
+def validate_phone(phone: str) -> str:  # Проверка номер на валидность
+    phone = re.sub(r'\D', '', phone)
+    return phone
 
 
-def calculate_age(dob: str) -> int:                      # Определение возраста на данный момент
+def calculate_age(dob: str) -> int:  # Определение возраста на данный момент
     birthdate = datetime.datetime.strptime(dob, '%d.%m.%Y')
     today = datetime.datetime.now()
     age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
     return age
 
+
 def read_content():
-    encoding = detect_encoding(filename)
-    with open(filename, 'r', encoding=encoding) as f:
+    encoding = detect_encoding(FILENAME)
+    with open(FILENAME, 'r', encoding=encoding) as f:
         return f.readlines()
 
 
-def phone_numbers_statistic(content):                           # Статистика по одинаковым номерам в консоли в виде таблицы
+def after_read_statistic(content):
     phones = []
-    for line in content:
-        phone = line.strip().split(';')[0]
+    surnames = []
+    years = {}
+    reader = csv.reader(content, delimiter=';')
+    for line in reader:
+        fullname = line[4]
+        surname = fullname.split(' ')[0]
+        surnames.append(surname)
+        phone = validate_phone(line[0])
         phones.append(phone)
-    duplicate_phones = set([phone for phone in phones if phones.count(phone) > 1])
+        date_string = line[8]
+        date_obj = datetime.datetime.strptime(date_string, "%d.%m.%Y")
+        year = date_obj.year
+        if year in years:
+            years[year] += 1
+        else:
+            years[year] = 1
+    same_surname_count = sum(count for count in Counter(surnames).values() if count > 1)
+    duplicate_phones = set(phone for phone in phones if phones.count(phone) > 1)
     num_duplicate_phones = len(duplicate_phones)
-    table_num = PrettyTable()
-    table_num.field_names = ["Количество повторяющихся номеров телефона", "Повторяющиеся номера"]
-    table_num.add_row([num_duplicate_phones, duplicate_phones])
-    print(table_num)
+    table = PrettyTable()
+    table.field_names = ["Количество повторяющихся номеров",
+                         "Повторяющиеся номера", "Количество однофамильцев"]
+    table.add_row([num_duplicate_phones, ", ".join(duplicate_phones), same_surname_count])
+    print(table)
+    table2 = PrettyTable()
+    table2.field_names = ["Год", "Количество родившихся"]
+    for year, count in sorted(years.items()):
+        table2.add_row([year, count])
+    print(table2)
 
 
-def surname_statistic(content):                                 # Статистика по однофамильцам в консоли в виде таблицы
-        surnames = []
-        for line in content:
-            surname = line.split()[4]
-            surnames.append(surname)
-        surname_counts = Counter(surnames)
-        same_surname_count = 0
-        for surname, count in surname_counts.items():
-            if count > 1:
-                same_surname_count += count
-        table_sur = PrettyTable()
-        table_sur.field_names = ["Количество однофамильцев"]
-        table_sur.add_row([same_surname_count])
-        print(table_sur)
+def sort_in_files(content):  # Считываем файл, раскидываем по двум файлам и выводим некорректные номера
+    encoding = detect_encoding(FILENAME)
+    reader = csv.reader(content, delimiter=';')
+    for i, line in enumerate(reader, start=1):
+        phone = line[0]
+        name_surname = line[3]
+        initials = line[4]
+        payment_method = line[7]
+        dob = line[8]
+        age = calculate_age(line[8])
+        formatted_line = f'ФИО: {initials}; Телефон: {validate_phone(phone)}; Дата Рождения: {dob}; Возраст на сегодня: {age}; \n'
+        if len(validate_phone(phone)) != NUMBERS_OF_DIGITS_IN_PHONE:
+            defect_line = f'{i} - ИО: {name_surname}; Телефон: {validate_phone(phone)};'
+            print(defect_line)
+        elif validate_phone(phone) and payment_method == 'pos':
+            with open("pos_h.csv", "a", encoding=encoding) as f:
+                f.write(formatted_line)
+        elif validate_phone(phone) and payment_method == 'cash':
+            with open("cash_h.csv", "a", encoding=encoding) as f:
+                f.write(formatted_line)
 
 
-def dob_statistic(content):                                    # Статистика по годам рождения в консоли в виде таблицы
-        years = {}
-        for line in content:
-            date_string = line.strip().split(';')[8]
-            date_obj = datetime.datetime.strptime(date_string, "%d.%m.%Y")
-            year = date_obj.year
-            if year in years:
-                years[year] += 1
-            else:
-                years[year] = 1
-        print("Статистика по годам рождения:")
-        table = PrettyTable()
-        table.field_names = ["Год", "Количество"]
-        for year, count in sorted(years.items()):
-            count = years[year]
-            table.add_row([year, count])
-        print(table)
-
-
-def read_file():                             # Считываем файл, раскидываем по двум файлам и выводим некорректные номера
-    encoding = detect_encoding(filename)
-    with open(filename, 'r', encoding=encoding) as f:
-        reader = csv.reader(f, delimiter=';')
-        for i, line in enumerate(reader, start=1):
-            formated_line = f'ФИО: {line[4]}; Телефон: {line[0]}; Дата Рождения: {line[8]}; Возраст на сегодня: {calculate_age(line[8])}; \n'
-            if not validate_phone(line[0]):
-                defect_line = f'{i} - ИО: {line[3]}; Телефон: {line[0]};'
-                print(defect_line)
-            elif validate_phone(line[0]) and line[7] == 'pos':
-                with open("pos_h.csv", "a", encoding=encoding) as f:
-                    f.write(formated_line)
-            elif validate_phone(line[0]) and line[7] == 'cash':
-                with open("cash_h.csv", "a", encoding=encoding) as f:
-                    f.write(formated_line)
-
-
-def delete():                                           # Удаляем файл после манипуляций
-    os.remove(filename)
+def delete():  # Удаляем файл после манипуляций
+    os.remove(FILENAME)
 
 
 def main():
     download_file()
     content = read_content()
-    read_file()
-    phone_numbers_statistic(content)
-    surname_statistic(content)
-    dob_statistic(content)
+    sort_in_files(content)
+    after_read_statistic(content)
     delete()
 
 
